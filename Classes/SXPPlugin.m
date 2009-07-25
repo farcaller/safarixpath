9d03add20ede86a0d8d2381454a81d3b1e79e1cb
Vladimir Pouzanov <farcaller@gmail.com>
2009-07-25 16:04:11 +0300

    Added MIT license header

--- Classes/SXPPlugin.m
+++ Classes/SXPPlugin.m
@@ -1,10 +1,25 @@
-//
-//  SXPPlugin.m
-//  SafariXPath
-//
-//  Created by Farcaller on 25.07.09.
-//  Copyright 2009 Hack&Dev FSO. All rights reserved.
-//
+/*****************************************************************************
+* Safari XPath                                                               *
+* Copyright (c) 2009 Vladimir "Farcaller" Pouzanov <farcaller@gmail.com>     *
+*                                                                            *
+* Permission is hereby granted, free of charge, to any person obtaining a    *
+* copy of this software and associated documentation files (the "Software"), *
+* to deal in the Software without restriction, including without limitation  *
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+* and/or sell copies of the Software, and to permit persons to whom the      *
+* Software is furnished to do so, subject to the following conditions:       *
+*                                                                            *
+* The above copyright notice and this permission notice shall be included in *
+* all copies or substantial portions of the Software.                        *
+*                                                                            *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS    *
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                 *
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.     *
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY       *
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,       *
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE          *
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                     *
+*****************************************************************************/
 
 #import "SXPPlugin.h"
 #import <Foundation/NSObjCRuntime.h>
@@ -66,6 +81,7 @@ static NSArray *webView_contextMenuItemsForElement_defaultMenuItems_(id self, SE
 		
 		_myMenuItem = [[NSMenuItem alloc] initWithTitle:@"XPath" action:nil keyEquivalent:@""];
 		[_myMenuItem setSubmenu:m];
+		[m release];
 		[_myMenuItem setEnabled:YES];
 		
 		// init ui
